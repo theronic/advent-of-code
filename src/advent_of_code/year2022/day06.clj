fc8419a7ed0cafb378b41f42b6589b1996fb3008
Petrus Theron <github@petrus.co.za>
2022-12-06 11:07:21 +0200

    add inline unit tests (using Hyperfiddle RCF) for Year 2022 Day 6

--- src/advent_of_code/year2022/day06.clj
+++ src/advent_of_code/year2022/day06.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code.year2022.day06)
+(ns advent-of-code.year2022.day06
+  (:require [hyperfiddle.rcf :refer (tests)]))
 
 (defn solve [n line]
   (->> (partition n 1 line)
@@ -8,6 +9,14 @@
           (+ idx n))))
     (first)))
 
-(comment
-  (solve 4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-  (solve 14 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+(tests
+  (solve 4 "bvwbjplbgvbhsrlpgdmjqwftvncz") := 5
+  (solve 4 "nppdvjthqldpwncqszvftbrmjlhg") := 6
+  (solve 4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") := 10
+  (solve 4 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") := 11
+
+  (solve 14 "mjqjpqmgbljsphdztnvjfqwrcgsmlb") := 19
+  (solve 14 "bvwbjplbgvbhsrlpgdmjqwftvncz") := 23
+  (solve 14 "nppdvjthqldpwncqszvftbrmjlhg") := 23
+  (solve 14 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") := 29
+  (solve 14 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") := 26)
